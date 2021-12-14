e1e084967832863e696e39e8761e6eead70e2d28
IlhamTahir <yantaq@bilig.biz>
2021-12-14 13:45:51 +0800

    first commit

---
+++ src/main/java/com/boyinmedia/music/BoyinMusicApplication.java
@@ -0,0 +1,13 @@
+package com.boyinmedia.music;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoyinMusicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoyinMusicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/boyinmedia/music/BoyinMusicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boyinmedia.music;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoyinMusicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
